51d2414c93d0cfc1e4fc813442ae7cf917dd457b
Toesmash <tommy.chvalny@gmail.com>
2018-03-09 14:56:24 +0100

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
 const store = configureStore();
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
